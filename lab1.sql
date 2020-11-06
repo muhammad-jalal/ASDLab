69785998879c4d1fabe712a00ed73ae31c889acf
muhammad-jalal <44603882+muhammad-jalal@users.noreply.github.com>
2020-11-06 15:18:58 +0530

    Create lab1.sql

---
+++ lab1.sql
@@ -0,0 +1,28 @@
+CREATE TABLE student
+(   sno INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    marks INT NOT NULL,
+    dept VARCHAR(20) NOT NULL
+);
+
+
+ALTER TABLE student 
+ADD COLUMN age INT NOT NULL;
+    
+    
+ALTER TABLE student 
+MODIFY COLUMN dept VARCHAR(10);  
+
+
+ALTER TABLE student 
+DROP COLUMN marks;
+
+
+RENAME TABLE student TO students;
+
+
+TRUNCATE TABLE students;
+
+
+DROP TABLE students;
+
