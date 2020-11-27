a3d0f0f5a34904d13dbddb832c3748424fa92461
muhammad-jalal <44603882+muhammad-jalal@users.noreply.github.com>
2020-11-27 14:01:23 +0530

    Update expt4.sql

--- expt4.sql
+++ expt4.sql
@@ -4,15 +4,26 @@ CREATE TABLE `department`
   `Title` VARCHAR(45) NOT NULL,
   `Dept_name` VARCHAR(45) NOT NULL UNIQUE,
   `Dept_id` INT NOT NULL,
-  `Salary` INT NOT NULL Check (Salary>2000),
+  `Salary` INT UNSIGNED NOT NULL Check (Salary>2000),
   PRIMARY KEY (`Code`)
 );
 
 
 CREATE TABLE `instructor` 
 (
-  `Id` INT NOT NULL DEFAULT 1,
   `Name` VARCHAR(45) NOT NULL,
   `Code` INT NOT NULL,
-   FOREIGN KEY (`Code`) REFERENCES `department` (`Code`)
+  `Id` INT PRIMARY KEY DEFAULT 1  
 );
+
+
+
+
+INSERT INTO `asd-lab`.`department`(`Code`,`Title`,`Dept_name`,`Dept_id`,`Salary`)
+VALUES (1,'TEACHER','COMPUTER',11,25000),
+	     (2,'LAB TECHNICIAN','ELECTRONICS',22,20500),
+       (3,'LAB ASSISTANT','ELECTRICAL',33,18500);
+       
+       
+INSERT INTO Instructor(`name`, `code`)
+VALUES ("RAMESH", 111);       
