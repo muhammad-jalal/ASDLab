2404de620e2210ec502be47f9ec5f3a229fe7b98
muhammad-jalal <44603882+muhammad-jalal@users.noreply.github.com>
2020-12-04 15:48:31 +0530

    Create expt5.sql

---
+++ expt5.sql
@@ -0,0 +1,33 @@
+CREATE TABLE Class (
+	  `id` INT PRIMARY KEY,
+    `name` VARCHAR(50)
+);
+
+
+
+INSERT INTO Class (`id`,`name`) 
+VALUES (1,"TIMON"), 
+       (2,"PUMBA"), 
+       (3,"EVA");
+
+
+
+SELECT * FROM Class;
+
+
+
+START TRANSACTION;
+
+INSERT INTO Class (`id`,`name`) VALUES (4,"WALL-E");
+ROLLBACK; 
+
+INSERT INTO Class (`id`,`name`) VALUES (4,"WALL-E");
+COMMIT;
+
+START TRANSACTION;
+
+SAVEPOINT A;
+INSERT INTO Class (`id`,`name`) VALUES (5,"SIMBA");
+ROLLBACK TO A;
+
+COMMIT;
