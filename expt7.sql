4b0ff2aae1a8b32e99b333299b6d8f474e33c702
muhammad-jalal <44603882+muhammad-jalal@users.noreply.github.com>
2020-12-18 17:04:53 +0530

    Create expt7.sql

---
+++ expt7.sql
@@ -0,0 +1,28 @@
+CREATE TABLE Store (
+	`order_no` INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `item` VARCHAR(20) NOT NULL,
+    `quantity` INTEGER UNSIGNED NOT NULL,
+    `discount` DECIMAL(2,2) DEFAULT(0),
+    `mrp` DECIMAL(6, 2) NOT NULL
+);
+
+
+INSERT INTO Store (`item`, `quantity`, `discount`, `mrp`)
+VALUES ('TEXT', 5, 0, 50), 
+       ('', 100, 0, 1);
+
+
+
+SELECT * FROM Store;
+
+
+
+SELECT MOD(`mrp`, 9) FROM Store;
+
+
+
+SELECT `mrp`, POWER(`mrp`, 2) FROM Store;
+
+
+
+SELECT ROUND(`mrp` / 7, 0) FROM Store;
